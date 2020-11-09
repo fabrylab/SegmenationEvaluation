b52f06b92e76dab26a44ff68d27b11d94351eb4a
Andreas <Andreas.Bauer2@gmx.net>
2020-11-09 09:59:41 +0100

    added opencv-python to requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(name='addon_seg_evaluation',
       version="0.1",
       packages=['addon_seg_evaluation'],
- 	install_requires=['clickpoints','tensorflow','deformationcytometer']
+ 	install_requires=['clickpoints', 'tensorflow', 'deformationcytometer', 'opencv-python']
       )
